1f06a4147821242296648331da9765cbf1634f51
riccardo <riccardogiuseppe.buffa@mail.polimi.it>
2025-07-11 17:38:12 +0200

    new

--- TIWProjectJS/src/main/java/it/polimi/dao/DatabaseConnection.java
+++ TIWProjectJS/src/main/java/it/polimi/dao/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/aste_online";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "Ulisse2003?";
+    private static final String PASSWORD = "ricky2003";
 
     private static final String FULL_URL = URL +
             "?useSSL=false" +
